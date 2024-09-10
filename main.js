23830901784bfca9cbb6861db81bd8765ace4a39
KurmanbekFront <nurkuluulukurmanbek53@gmail.com>
2024-09-10 22:03:15 +0600

    DZ3

---
+++ main.js
@@ -0,0 +1,54 @@
+// задание 1
+
+// const min = (num1, num2) => {
+//     if (num1 < num2) {
+//         return num1
+//     } else {
+//         return num2
+//     }
+// }
+// console.log(min(20, 15));
+// console.log(min(5, 10));
+
+
+// задание 2
+
+// let str = prompt().toLocaleLowerCase().trim()
+
+// const stringLength = (str) => {
+//     return str.length
+// }
+// console.log(stringLength(str));
+
+
+// задание 3
+
+// const calculator = (num1, num2, operator) => {
+//     switch (operator) {
+//         case "+":
+//             console.log(num1 + num2);
+//             break;
+//         case "-":
+//             console.log(num1 - num2);
+//             break;
+//         case "*":
+//             console.log(num1 * num2);
+//             break;
+//         case "/":
+//             if (num2 === 0) {
+//                 console.log("делить на ноль нельзя");
+//             } else {
+//                 console.log(num1 / num2);
+//             }
+//             break;
+//         default:
+//             console.log("Ошибка: неверная операция");
+//     }
+// }
+// calculator(10, 5, '+'); 
+// calculator(10, 5, '-');
+// calculator(10, 5, '*'); 
+// calculator(10, 5, '/');
+// calculator(10, 0, '/');
+// calculator(10, 5, '^');
+
